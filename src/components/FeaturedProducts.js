7371fe61d64522edbdbaa386416b69dcb46e52a8
Sankalpa Neupane <sankalpa115@gmail.com>
2022-07-04 13:00:36 +0545

    silly mistakes prabin

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -15,9 +15,9 @@ function FeaturedProducts({products}) {
       <Fade left>
       <div className="lg:h-64 h-60  py-2 flex  overflow-scroll space-x-3 scrollbar-hide">
           {featuredProducts?.slice(0,15).map((product) => (
-            <Link to={`/product/${product.id}`}>
+            <Link key={product.id} to={`/product/${product.id}`}>
               <section
-                key={product.id}
+                
                 className="h-full w-60  cursor-pointer shrink-0"
               >
                 <img src={product?.image} className="h-full w-full object-cover"/>
